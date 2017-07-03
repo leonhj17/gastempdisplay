098d097b3746108d3672e988c6619469c2f41147
huangjian <huangjian17@outlook.com>
2017-07-03 14:23:40 +0800

    create gas models

---
+++ gas/models.py
@@ -0,0 +1,16 @@
+# _*_ encoding:utf-8 _*_
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+class Temp(models.Model):
+    loc_gz = models.CharField(choices=(('ggq', u'高过前'), ('gzq', u'高再前'), ('gzh', u'高再后')),
+                              verbose_name=u'烟温探针位置', blank=False, null=False, max_length=5)
+    loc_umd = models.CharField(choices=(('up', u'上'), ('middle', u'中'), ('down', u'下')),
+                               verbose_name=u'高度', blank=False, null=False, max_length=5)
+    loc_ab = models.CharField(choices=(('a', u'左侧'), ('b', u'右侧')),
+                              verbose_name=u'左右侧', blank=False, null=False, max_length=5)
+    temp = models.FloatField(verbose_name=u'测量温度', max_length=1400, blank=True, null=True)
+    case_time = models.DateTimeField(verbose_name=u'测量工况时间')
--- gastempdisplay/settings.py
+++ gastempdisplay/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gas',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'gastempdisplay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "gastempdisplay",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
