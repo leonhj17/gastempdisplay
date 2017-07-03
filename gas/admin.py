22fe2e8fdec9195b671526f16c2f66f2512a6c42
huangjian <huangjian17@outlook.com>
2017-07-03 14:38:59 +0800

    create Temp admin

--- gas/admin.py
+++ gas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Temp
+
+
+class TempAdmin(admin.ModelAdmin):
+    list_display = ['loc_gz', 'loc_umd', 'loc_ab', 'temp', 'case_time']
+    list_filter = ['loc_gz', 'loc_umd', 'loc_ab', 'temp', 'case_time']
+    search_fields = ['loc_gz', 'loc_umd', 'loc_ab', 'temp']
+
+
+admin.site.register(Temp, TempAdmin)
